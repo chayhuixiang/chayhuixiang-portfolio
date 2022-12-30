1a3d80fdf497ef34f23c71b2d310741a72250635
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-12-30 12:31:11 +0800

    fix email text

--- app/components/images/contact/Done.tsx
+++ app/components/images/contact/Done.tsx
@@ -3,7 +3,10 @@ import { CheckIcon } from '@heroicons/react/24/outline'
 
 const Done = () => {
   return (
-    <CheckIcon className='w-5 h-5 stroke-2' />
+    <>
+      <CheckIcon className='w-5 h-5 stroke-2' />
+      <p className='font-bold m-auto text-sm sm:text-base'>Thanks for reaching out!</p>
+    </>
   )
 }
 

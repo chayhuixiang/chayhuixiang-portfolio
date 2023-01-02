6b8798a2683e8dfdbb416de0431c56e563617e94
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-02 17:40:40 +0800

    Revert graphql changes

--- lib/graphqlClient.ts
+++ lib/graphqlClient.ts
@@ -1,4 +1,4 @@
 import { GraphQLClient } from "graphql-request";
 
-const GQL_ENDPOINT = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/graphql' : `https://www.chayhuixiang.com/api/graphql`;
+const GQL_ENDPOINT = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/graphql' : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`;
 export const graphqlClient = new GraphQLClient(GQL_ENDPOINT);

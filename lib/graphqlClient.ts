a753f6b97d715d445bf39058f6ed79487f303dde
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-02 17:42:06 +0800

    Fix graphql url

--- lib/graphqlClient.ts
+++ lib/graphqlClient.ts
@@ -1,4 +1,4 @@
 import { GraphQLClient } from "graphql-request";
 
-const GQL_ENDPOINT = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/graphql' : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`;
+const GQL_ENDPOINT = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/graphql' : 'https://chayhuixiang.com/api/graphql';
 export const graphqlClient = new GraphQLClient(GQL_ENDPOINT);

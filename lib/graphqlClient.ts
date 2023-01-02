983c56a079285a9cbb493bde8da6782353e490fc
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-02 17:02:14 +0800

    Fix graphql

--- lib/graphqlClient.ts
+++ lib/graphqlClient.ts
@@ -1,3 +1,3 @@
 import { GraphQLClient } from "graphql-request";
 
-export const graphqlClient = new GraphQLClient(process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/graphql' : 'https://www.chayhuixiang.com/api/graphql');
+export const graphqlClient = new GraphQLClient(`${process.env.URL}/api/graphql`);

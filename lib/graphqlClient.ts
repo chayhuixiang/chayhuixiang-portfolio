cf5569feef84df41167fadc38ef699f7150d98c2
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-05 09:46:40 +0800

    debug graphql

--- lib/graphqlClient.ts
+++ lib/graphqlClient.ts
@@ -4,4 +4,5 @@ import { getAbsoluteUrl } from "../utils/getAbsoluteUrl";
 // const GQL_ENDPOINT = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/graphql' : 'https://chayhuixiang-portfolio.vercel.app/api/graphql';
 const absoluteUrl = getAbsoluteUrl();
 const GQL_ENDPOINT = `${absoluteUrl}/api/graphql`
+console.log(GQL_ENDPOINT);
 export const graphqlClient = new GraphQLClient(GQL_ENDPOINT);

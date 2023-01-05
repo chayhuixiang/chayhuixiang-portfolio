4ea96121a3440fe56f18f6f6c17fc0baa2afb740
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-05 09:52:36 +0800

    Fix graphql

--- lib/graphqlClient.ts
+++ lib/graphqlClient.ts
@@ -2,7 +2,10 @@ import { GraphQLClient } from "graphql-request";
 import { getAbsoluteUrl } from "../utils/getAbsoluteUrl";
 
 // const GQL_ENDPOINT = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api/graphql' : 'https://chayhuixiang-portfolio.vercel.app/api/graphql';
-const absoluteUrl = getAbsoluteUrl();
+let absoluteUrl = getAbsoluteUrl();
+if (!absoluteUrl) {
+  absoluteUrl = `https://${process.env.VERCEL_URL}`;
+}
 const GQL_ENDPOINT = `${absoluteUrl}/api/graphql`
 console.log(GQL_ENDPOINT);
 export const graphqlClient = new GraphQLClient(GQL_ENDPOINT);

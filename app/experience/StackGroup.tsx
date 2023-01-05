78d01a7c72012c49340328d0f42b42e524b48de6
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-05 09:18:57 +0800

    Fix StackGroup

--- app/experience/StackGroup.tsx
+++ app/experience/StackGroup.tsx
@@ -12,8 +12,8 @@ const StackGroup = ({sortedWorkStack}: Props) => {
   // const sortedWorkStack: Stack[] = fetchedWorkStack.sort((a, b) => a.skill_work_order - b.skill_work_order);
   return (
     <div className='grid xl:grid-cols-9 sm:grid-cols-5 grid-cols-3 gap-7'>
-      {sortedWorkStack.map(({name, logo_path_light, logo_path_dark, colour, stack_work_order}: Stack) => 
-        <StackCard name={name} logo_path_light={logo_path_light} logo_path_dark={logo_path_dark} colour={colour} key={stack_work_order} />)}
+      {sortedWorkStack.map(({name, logo_path_light, logo_path_dark, colour}: Stack, i) => 
+        <StackCard name={name} logo_path_light={logo_path_light} logo_path_dark={logo_path_dark} colour={colour} key={i} />)}
     </div>
   )
 }
